34c65ea5719e680317ec6544cb349859ddb23695
Luis Pellizzon <luis.pellizzon@gmail.com>
2025-11-27 15:07:45 +0000

    feat: gc to delete pandas data to reduce memory usage. Implement loader on web app

--- client/src/components/Loading.tsx
+++ client/src/components/Loading.tsx
@@ -1,11 +1,8 @@
-import {Film} from "lucide-react";
 import {LoadingBox} from "./LoadingBox";
-export const Loading: React.FC<{message?: string}> = ({
-  message = "Finding Perfect Movies for You",
-}) => {
+export const Loading: React.FC<{message?: string}> = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-linear-to-br from-purple-600 via-pink-500 to-red-500">
-      <LoadingBox />
+      <LoadingBox message="Finding Perfect Movies for You" />
     </div>
   );
 };
